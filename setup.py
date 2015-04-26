3689c9e5d52d5b4d8aeaa2dd8375fcdfdf71f7a6
Samu Kallio <samu@samukallio.net>
2015-04-26 15:37:17 +0300

    Add basic setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(name='s3pac',
+      version='0.1',
+      description='Pacman repository server built on Amazon SimpleDB and S3',
+      author='Samu Kallio',
+      author_email='samu@samukallio.net',
+      url='https://github.com/samukallio/s3pac',
+      packages=['s3pac'],
+     )
